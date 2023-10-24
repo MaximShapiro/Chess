90a81514eb60fe2ee467670d345dfae92fdec2db
MaximShapiro <Max.Shapiro@web.de>
2023-10-24 13:53:56 +0300

    Chess piece instantiated

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    public GameObject chessPiece;
     // Start is called before the first frame update
     void Start()
     {
-        
+      Instantiate(chessPiece, new Vector3(0,0,-1), Quaternion.identity);  
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
